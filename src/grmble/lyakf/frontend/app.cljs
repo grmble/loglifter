daee3603906b05460bae6c257052e9838a155439
Juergen Gmeiner <spamless.juergen@gmail.com>
2022-10-19 20:07:37 +0200

    bulma page layout, simple reagent app

--- src/grmble/lyakf/frontend/app.cljs
+++ src/grmble/lyakf/frontend/app.cljs
@@ -3,6 +3,31 @@
    [reagent.core :as r]
    [reagent.dom :as rdom]))
 
+(defonce session (r/atom nil))
+
+(defn nav-link [uri title page]
+  [:a.navbar-item
+   {:href   uri
+    :class (when (= page (:page @session)) "is-active")}
+   title])
+
+(defn navbar []
+  (r/with-let [expanded? (r/atom false)]
+    [:nav.navbar.is-info>div.container
+     [:div.navbar-brand
+      [:a.navbar-item {:href "/" :style {:font-weight :bold}} "Learn You A Kee-Frame"]
+      [:span.navbar-burger.burger
+       {:data-target :nav-menu
+        :on-click #(swap! expanded? not)
+        :class (when @expanded? :is-active)}
+       [:span] [:span] [:span]]]
+     [:div#nav-menu.navbar-menu
+      {:class (when @expanded? :is-active)}
+      [:div.navbar-start
+       [nav-link "#/" "Home" :home]
+       [nav-link "#/about" "About" :about]]]]))
+
+
 (def click-count (r/atom 0))
 
 (defn counting-component []
@@ -16,6 +41,10 @@
    [:input {:type "button" :value "Dec"
             :on-click #(swap! click-count dec)}]])
 
+(defn main-page []
+  [:<>
+   (navbar)
+   (counting-component)])
 
 (defn- root-element []
   (.getElementById js/document "root"))
@@ -24,7 +53,7 @@
 ;; after-load! is called after every load
 (defn ^:dev/after-load after-load! []
   ;; note the use of the var - #' reader syntax
-  (rdom/render [#'counting-component] (root-element)))
+  (rdom/render [#'main-page] (root-element)))
 (defn init! []
   (after-load!)
   (println "init! complete"))
