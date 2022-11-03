3d90d84b0bd066c9f4ff0d69bc43b429ecdcdb3a
Juergen Gmeiner <spamless.juergen@gmail.com>
2022-11-03 21:36:34 +0100

    dead branch "the pouchening"

---
+++ src/grmble/lyakf/frontend/storage/foreign.cljs
@@ -0,0 +1,67 @@
+(ns grmble.lyakf.frontend.storage.foreign
+  "Convert app db parts into js objects.
+
+   This is very ugly.  I have plans, but for now we will live
+   with the ugly.
+   "
+  (:require
+   [grmble.lyakf.frontend.model.exercise :as exercise]
+   [grmble.lyakf.frontend.model.program :as program]
+   [medley.core :as medley]))
+
+
+(defn- js->any
+  "Convert a JSON object to a clojure type"
+  [obj]
+  (js->clj obj {:keywordize-keys true}))
+
+(def exercises->js clj->js)
+(def programs->js clj->js)
+
+(defn js->exercises [obj]
+  (->> obj
+       js->any
+       (medley/map-vals #(exercise/map->Exercise
+                          (update % :slug keyword)))))
+
+(defn- process-workouts [ws]
+  (mapv (fn [w]
+          (mapv (fn [x]
+                  (if (map? x)
+                    (program/json->xref x)
+                    (mapv program/json->xref x)))
+                w))
+        ws))
+
+(defn js->programs [obj]
+  (->> obj
+       js->any
+       (medley/map-vals #(program/map->Program
+                          (-> %
+                              (update :slug keyword)
+                              (update :workouts process-workouts))))))
+
+(defn- encode-data [data]
+  (for [[w xs] data]
+    [w (for [[x m] xs]
+         [x m])]))
+
+(defn- decode-data [data]
+  (into {} (for [[w xs] data]
+             [w
+              (into {} (for [[x m] xs]
+                         [x m]))])))
+
+
+(defn current->js [{:keys [data slug] :as current}]
+  (-> current
+      (assoc :data (encode-data data)
+             :slug (name slug))
+      (clj->js)))
+
+(defn js->current [obj]
+  (let [{:keys [data slug] :as current} (js->any obj)]
+    (-> current
+        (assoc :data (decode-data data)
+               :slug (keyword slug)))))
+
---
+++ test/grmble/lyakf/frontend/storage/foreign_test.cljs
@@ -0,0 +1,43 @@
+(ns grmble.lyakf.frontend.storage.foreign-test
+  (:require
+   [cljs.test :refer [deftest testing is]]
+   [cljs.spec.alpha :as s]
+   [grmble.lyakf.frontend.model.exercise :as exercise]
+   [grmble.lyakf.frontend.model.program :as program]
+   [grmble.lyakf.frontend.model]
+   [grmble.lyakf.frontend.model.util]
+   [grmble.lyakf.frontend.storage.foreign :as foreign]))
+
+
+(defn valid-or-explain [schema v]
+  (if (s/valid? schema v)
+    true
+    (s/explain schema v)))
+
+(deftest exercises
+  (testing "roundtrip"
+    (let [obj  (foreign/exercises->js exercise/default-exercises)
+          v    (foreign/js->exercises obj)]
+      (is (valid-or-explain :grmble.lyakf.frontend.model/exercises v))
+      (is (= exercise/default-exercises v)))))
+
+(deftest programs
+  (testing "roundtrip"
+    (let [obj  (foreign/programs->js program/default-programs)
+          v    (foreign/js->programs obj)]
+      (is (valid-or-explain :grmble.lyakf.frontend.model/programs v))
+      (is (= program/default-programs v)))))
+
+(defn- current-roundtrip [data]
+  (let [obj  (foreign/current->js data)
+        v    (foreign/js->current obj)]
+    (is (valid-or-explain :grmble.lyakf.frontend.model/current v))
+    (is (= data v))))
+
+(deftest current-data
+  (testing "roundtrip"
+    (current-roundtrip {:slug :xxx
+                        :data {}})
+    (current-roundtrip
+     {:slug :xxx
+      :data {0 {0 {:completed "xxx"}}}})))
